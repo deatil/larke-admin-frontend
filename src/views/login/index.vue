8e930b93434f5c2117af7f2d02e010125610af40
deatil <2217957370@qq.com>
2024-07-15 12:29:44 +0800

    优化登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,85 +1,90 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
+    <div class="login-main">
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
-      <div class="title-container">
-        <h3 class="title">
-          {{ $t('login.title') }}
-        </h3>
-        <lang-select class="set-language" />
-      </div>
+        <div class="title-container">
+          <h3 class="title">
+            {{ $t('login.title') }}
+          </h3>
+          <lang-select class="set-language" />
+        </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          :placeholder="$t('login.username')"
-          name="username"
-          type="text"
-          tabindex="1"
-          autocomplete="off"
-        />
-      </el-form-item>
-
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-        <el-form-item prop="password">
+        <el-form-item prop="username">
           <span class="svg-container">
-            <svg-icon icon-class="password" />
+            <svg-icon icon-class="user" />
           </span>
           <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            :placeholder="$t('login.password')"
-            name="password"
-            tabindex="2"
+            ref="username"
+            v-model="loginForm.username"
+            :placeholder="$t('login.username')"
+            name="username"
+            type="text"
+            tabindex="1"
             autocomplete="off"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
-            @keyup.enter.native="handleLogin"
           />
-          <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
         </el-form-item>
-      </el-tooltip>
 
-      <el-form-item prop="captcha">
-        <span class="svg-container">
-          <i class="el-icon-picture-outline" />
-        </span>
-        <el-input
-          ref="captcha"
-          v-model="loginForm.captcha"
-          :placeholder="$t('login.captcha')"
-          name="captcha"
-          type="text"
-          tabindex="3"
-          autocomplete="off"
-          class="captcha-input"
-        />
-
-        <el-tooltip effect="light" :content="$t('login.refreshCaptchaTip')" placement="top">
-          <span class="captcha-img" @click="refreshCaptcha">
-            <img :src="captchaImg" :title="$t('login.refreshCaptcha')">
-          </span>        
+        <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="password"
+              v-model="loginForm.password"
+              :type="passwordType"
+              :placeholder="$t('login.password')"
+              name="password"
+              tabindex="2"
+              autocomplete="off"
+              @keyup.native="checkCapslock"
+              @blur="capsTooltip = false"
+              @keyup.enter.native="handleLogin"
+            />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
         </el-tooltip>
-      </el-form-item>
 
-      <el-button 
-        :loading="loading" 
-        type="primary" 
-        style="width:100%;margin-bottom:30px;" 
-        @click.native.prevent="handleLogin">
-        {{ $t('login.logIn') }}
-      </el-button>
+        <el-form-item prop="captcha">
+          <span class="svg-container">
+            <i class="el-icon-picture-outline" />
+          </span>
+          <el-input
+            ref="captcha"
+            v-model="loginForm.captcha"
+            :placeholder="$t('login.captcha')"
+            name="captcha"
+            type="text"
+            tabindex="3"
+            autocomplete="off"
+            class="captcha-input"
+          />
 
-    </el-form>
+          <el-tooltip effect="light" :content="$t('login.refreshCaptchaTip')" placement="top">
+            <span class="captcha-img" @click="refreshCaptcha">
+              <img :src="captchaImg" :title="$t('login.refreshCaptcha')">
+            </span>        
+          </el-tooltip>
+        </el-form-item>
 
+        <el-button 
+          :loading="loading" 
+          type="primary" 
+          style="width:100%;margin-bottom:35px;" 
+          @click.native.prevent="handleLogin">
+          {{ $t('login.logIn') }}
+        </el-button>
+
+      </el-form>
+
+      <div class="copyright">
+          Copyright @ 2024 <a target="_blank" href="http://github.com/deatil/larke-admin">Larke-admin</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -300,14 +305,39 @@ $light_gray:#eee;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  background: url(../../assets/larke/loginbg.png) 0% 0% / cover no-repeat;
+  position: static;
   overflow: hidden;
+
+  .login-main {
+    width: 428px;
+    position: relative;
+    margin: 0 auto;
+    margin-top: 80px;
+    margin-bottom: 50px;
+    background: #526070;
+    border-radius: 5px;
+
+    .copyright {
+        color: #fff;
+        width: 100%;
+        position: absolute;
+        text-align: center;
+        line-height: 30px;
+        padding: 10px 0;
+        text-shadow: #000 0.1em 0.1em 0.1em;
+        font-size: 14px;
+    }
+    .copyright a, .copyright span {
+        color: #fff;
+    }
+  }
 
   .login-form {
     position: relative;
     width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    padding: 35px 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
@@ -392,4 +422,5 @@ $light_gray:#eee;
     }
   }
 }
+
 </style>
